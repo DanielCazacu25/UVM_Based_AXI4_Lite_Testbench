b02ac101128a3035c8478e6bcee8b9d2d21d9ef5
Daniel <danielcazacu205@gmail.com>
2026-09-08 12:32:49 +0300

    add:wr sequence

---
+++ Testbench/AXI4Lite_wr_seq.sv
@@ -0,0 +1,33 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+class AXI4Lite_wr_seq extends uvm_sequence #(AXI4Lite_wr_item);
+
+    `uvm_object_utils(AXI4Lite_wr_seq)
+
+    int num_trans = 1000;
+
+    function new(string name = "AXI4Lite_wr_seq");
+
+        super.new(name);
+
+    endfunction
+
+    task body();
+
+        AXI4Lite_wr_item wr_itm;
+
+        repeat(num_trans) begin
+            
+            wr_itm = AXI4Lite_wr_item :: type_id :: create("wr_itm");
+
+            start_item(wr_itm);
+
+            assert(wr_itm.randomize());
+
+            finish_item(wr_itm);
+            
+        end
+      
+    endtask
+endclass
